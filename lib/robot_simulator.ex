baf0aeae4ff145b6c9389eebf2978fc03ecfc847
Josh Carrier <joshjcarrier@gmail.com>
2018-02-17 19:51:10 -0800

    Arrange state by direction and implement with protocol. Will be fine if direction stays the main variation in behavior.

--- lib/robot_simulator.ex
+++ lib/robot_simulator.ex
@@ -1,87 +1,146 @@
-defmodule Robot do
-  defstruct bearing: nil, position: nil
+defprotocol Robot do
+  def advance(robot)
 
-  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :north do
-    %Robot{robot | position: {x, y + 1}}
+  def direction(robot)
+
+  def position(robot)
+
+  def turn_left(robot)
+
+  def turn_right(robot)
+end
+
+defmodule Robot.North do
+  defstruct [:position]
+end
+
+defmodule Robot.South do
+  defstruct [:position]
+end
+
+defmodule Robot.East do
+  defstruct [:position]
+end
+
+defmodule Robot.West do
+  defstruct [:position]
+end
+
+defimpl Robot, for: Robot.North do
+  def advance(%{position: {x, y}} = robot) do
+    %{robot | position: {x, y + 1}}
   end
 
-  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :south do
-    %Robot{robot | position: {x, y - 1}}
+  def direction(_robot) do
+    :north
   end
 
-  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :east do
-    %Robot{robot | position: {x + 1, y}}
-  end
-
-  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :west do
-    %Robot{robot | position: {x - 1, y}}
-  end
-
-  def create(bearing \\ :north, position \\ {0, 0})
-
-  def create(bearing, position)
-      when bearing in [:north, :south, :east, :west] and is_tuple(position) and
-             tuple_size(position) == 2 and is_integer(elem(position, 0)) and
-             is_integer(elem(position, 1)) do
-    {:ok, %Robot{bearing: bearing, position: position}}
-  end
-
-  def create(bearing, _position) when bearing not in [:north, :south, :east, :west] do
-    {:error, "invalid direction"}
-  end
-
-  def create(_bearing, _position) do
-    {:error, "invalid position"}
-  end
-
-  def direction(%Robot{bearing: bearing}) do
-    bearing
-  end
-
-  def position(%Robot{position: position}) do
+  def position(%{position: position}) do
     position
   end
 
-  def turn_left(%Robot{bearing: bearing, position: position}) when bearing == :north do
-    %Robot{bearing: :west, position: position}
+  def turn_left(%{position: position}) do
+    %Robot.West{position: position}
   end
 
-  def turn_left(%Robot{bearing: bearing, position: position}) when bearing == :south do
-    %Robot{bearing: :east, position: position}
+  def turn_right(%{position: position}) do
+    %Robot.East{position: position}
+  end
+end
+
+defimpl Robot, for: Robot.South do
+  def advance(%{position: {x, y}} = robot) do
+    %{robot | position: {x, y - 1}}
   end
 
-  def turn_left(%Robot{bearing: bearing, position: position}) when bearing == :east do
-    %Robot{bearing: :north, position: position}
+  def direction(_robot) do
+    :south
   end
 
-  def turn_left(%Robot{bearing: bearing, position: position}) when bearing == :west do
-    %Robot{bearing: :south, position: position}
+  def position(%{position: position}) do
+    position
   end
 
-  def turn_right(%Robot{bearing: bearing, position: position}) when bearing == :north do
-    %Robot{bearing: :east, position: position}
+  def turn_left(%{position: position}) do
+    %Robot.East{position: position}
   end
 
-  def turn_right(%Robot{bearing: bearing, position: position}) when bearing == :south do
-    %Robot{bearing: :west, position: position}
+  def turn_right(%{position: position}) do
+    %Robot.West{position: position}
+  end
+end
+
+defimpl Robot, for: Robot.East do
+  def advance(%{position: {x, y}} = robot) do
+    %{robot | position: {x + 1, y}}
   end
 
-  def turn_right(%Robot{bearing: bearing, position: position}) when bearing == :east do
-    %Robot{bearing: :south, position: position}
+  def direction(_robot) do
+    :east
   end
 
-  def turn_right(%Robot{bearing: bearing, position: position}) when bearing == :west do
-    %Robot{bearing: :north, position: position}
+  def position(%{position: position}) do
+    position
+  end
+
+  def turn_left(%{position: position}) do
+    %Robot.North{position: position}
+  end
+
+  def turn_right(%{position: position}) do
+    %Robot.South{position: position}
+  end
+end
+
+defimpl Robot, for: Robot.West do
+  def advance(%{position: {x, y}} = robot) do
+    %{robot | position: {x - 1, y}}
+  end
+
+  def direction(_robot) do
+    :west
+  end
+
+  def position(%{position: position}) do
+    position
+  end
+
+  def turn_left(%{position: position}) do
+    %Robot.South{position: position}
+  end
+
+  def turn_right(%{position: position}) do
+    %Robot.North{position: position}
   end
 end
 
 defmodule RobotSimulator do
-  def create do
-    Robot.create()
+  def create(bearing \\ :north, position \\ {0, 0})
+
+  def create(_bearing, position)
+      when not (is_tuple(position) and tuple_size(position) == 2 and is_integer(elem(position, 0)) and
+                  is_integer(elem(position, 1))) do
+    {:error, "invalid position"}
   end
 
-  def create(bearing, position) do
-    Robot.create(bearing, position)
+  def create(bearing, position) when bearing == :north do
+    {:ok, %Robot.North{position: position}}
+  end
+
+  def create(bearing, position) when bearing == :south do
+    {:ok, %Robot.South{position: position}}
+  end
+
+  def create(bearing, position) when bearing == :east do
+    {:ok, %Robot.East{position: position}}
+  end
+
+  def create(bearing, position) when bearing == :west do
+    {:ok, %Robot.West{position: position}}
+  end
+
+  def create(_bearing, _position) do
+    {:error, "invalid direction"}
   end
 
   def direction({_, robot}) do
