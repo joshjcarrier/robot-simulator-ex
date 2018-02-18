3d7d0d43b51b011af343bb6eeb41b31fd292ac8a
Josh Carrier <joshjcarrier@gmail.com>
2018-02-17 18:31:58 -0800

    Declarative creation of Robot

--- lib/robot_simulator.ex
+++ lib/robot_simulator.ex
@@ -1,20 +1,37 @@
 defmodule Robot do
   defstruct bearing: nil, position: nil
 
-  def advance(%Robot{bearing: bearing, position: {x, y}}) when bearing == :north do
-    %Robot{bearing: bearing, position: {x, y + 1}}
+  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :north do
+    %Robot{robot | position: {x, y + 1}}
   end
 
-  def advance(%Robot{bearing: bearing, position: {x, y}}) when bearing == :south do
-    %Robot{bearing: bearing, position: {x, y - 1}}
+  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :south do
+    %Robot{robot | position: {x, y - 1}}
   end
 
-  def advance(%Robot{bearing: bearing, position: {x, y}}) when bearing == :east do
-    %Robot{bearing: bearing, position: {x + 1, y}}
+  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :east do
+    %Robot{robot | position: {x + 1, y}}
   end
 
-  def advance(%Robot{bearing: bearing, position: {x, y}}) when bearing == :west do
-    %Robot{bearing: bearing, position: {x - 1, y}}
+  def advance(%Robot{bearing: bearing, position: {x, y}} = robot) when bearing == :west do
+    %Robot{robot | position: {x - 1, y}}
+  end
+
+  def create(bearing \\ :north, position \\ {0, 0})
+
+  def create(bearing, position)
+      when bearing in [:north, :south, :east, :west] and is_tuple(position) and
+             tuple_size(position) == 2 and is_integer(elem(position, 0)) and
+             is_integer(elem(position, 1)) do
+    {:ok, %Robot{bearing: bearing, position: position}}
+  end
+
+  def create(bearing, _position) when bearing not in [:north, :south, :east, :west] do
+    {:error, "invalid direction"}
+  end
+
+  def create(_bearing, _position) do
+    {:error, "invalid position"}
   end
 
   def direction(%Robot{bearing: bearing}) do
@@ -59,21 +76,12 @@ defmodule Robot do
 end
 
 defmodule RobotSimulator do
-  def create(bearing \\ :north, position \\ {0, 0})
-
-  def create(bearing, position)
-      when bearing in [:north, :south, :east, :west] and is_tuple(position) and
-             tuple_size(position) == 2 and is_integer(elem(position, 0)) and
-             is_integer(elem(position, 1)) do
-    {:ok, %Robot{bearing: bearing, position: position}}
+  def create do
+    Robot.create()
   end
 
-  def create(bearing, _position) when bearing not in [:north, :south, :east, :west] do
-    {:error, "invalid direction"}
-  end
-
-  def create(_bearing, _position) do
-    {:error, "invalid position"}
+  def create(bearing, position) do
+    Robot.create(bearing, position)
   end
 
   def direction({_, robot}) do
@@ -90,23 +98,23 @@ defmodule RobotSimulator do
     |> Enum.reduce(robot_simulator, &execute_instruction/2)
   end
 
-  def execute_instruction(_instruction, {status, _robot}) when status == :error do
+  defp execute_instruction(_instruction, {status, _robot}) when status == :error do
     {:error, "invalid instruction"}
   end
 
-  def execute_instruction(instruction, {_, robot}) when instruction == "A" do
+  defp execute_instruction(instruction, {_, robot}) when instruction == "A" do
     {:ok, Robot.advance(robot)}
   end
 
-  def execute_instruction(instruction, {_, robot}) when instruction == "L" do
+  defp execute_instruction(instruction, {_, robot}) when instruction == "L" do
     {:ok, Robot.turn_left(robot)}
   end
 
-  def execute_instruction(instruction, {_, robot}) when instruction == "R" do
+  defp execute_instruction(instruction, {_, robot}) when instruction == "R" do
     {:ok, Robot.turn_right(robot)}
   end
 
-  def execute_instruction(_instruction, _robot) do
+  defp execute_instruction(_instruction, _robot) do
     {:error, "invalid instruction"}
   end
 end
